10810941ab0cad47c2e1ceafabaf191098e269cf
Miguel Lezama <miguel@automattic.com>
2018-03-07 14:50:56 -0300

    fix formatting

--- _inc/lib/class.jetpack-user-event-tracking.php
+++ _inc/lib/class.jetpack-user-event-tracking.php
@@ -6,20 +6,22 @@ class Jetpack_User_Event_Tracking {
 	const KEY = 'jetpack_event_tracking_opted_out';
 
 	static function is_enabled( $user_id ) {
-			return ! self::is_disabled( $user_id );
+		return ! self::is_disabled( $user_id );
 	}
 
 	static function is_disabled( $user_id ) {
 		if ( isset( self::$_memorize_setting[ $user_id ] ) ) {
 			return self::$_memorize_setting[ $user_id ];
 		}
-		$user_tracking = get_user_meta( $user_id, self::KEY , true );
+		$user_tracking = get_user_meta( $user_id, self::KEY, true );
 		if ( is_numeric( $user_tracking ) ) {
 			self::$_memorize_setting[ $user_id ] = (bool) $user_tracking;
+
 			return (bool) $user_tracking;
 		}
-		$default = self::default_value();
+		$default                             = self::default_value();
 		self::$_memorize_setting[ $user_id ] = (bool) $default;
+
 		return (bool) self::default_value();
 	}
 
